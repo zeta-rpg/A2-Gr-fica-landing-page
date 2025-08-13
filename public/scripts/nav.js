2bf02fac937150f52b85db9e411fd608754e5bf1
Samuel Campos <zckfr000@gmail.com>
2025-08-12 22:26:07 -0300

    El proyecto recien comienza
    git push -u origin main
    git commit -m "Subiendo proyecto completo"

---
+++ public/scripts/nav.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('active');
+});
